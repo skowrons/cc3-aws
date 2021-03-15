b901c6e485f8fc06113aa8cc7836e78a038a07ea
skowrons <m.skowron@mskowron.de>
2021-03-15 16:11:54 +0100

    added sms sending function

--- github-webhook-lambda-sns/sms-sender/go.mod
+++ github-webhook-lambda-sns/sms-sender/go.mod
@@ -2,4 +2,7 @@ module sms
 
 go 1.15
 
-require github.com/aws/aws-lambda-go v1.23.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.23.0 // indirect
+	github.com/aws/aws-sdk-go v1.37.30 // indirect
+)
--- github-webhook-lambda-sns/sms-sender/main.go
+++ github-webhook-lambda-sns/sms-sender/main.go
@@ -1,19 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
 )
 
-
+// PhoneNumber must be changed
+const PhoneNumber = "+49 176 12312312"
 
 func HandleRequest(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Println("HEllow SMS")
+	log.Println("Received a github event!")
 	log.Println(r.Body)
 
-	return events.APIGatewayProxyResponse{Body: "some unhandled method", StatusCode: 200}, nil
+	if strings.Contains(r.Body, "skowrons") {
+		log.Println("creating session")
+		sess := session.Must(session.NewSession())
+		log.Println("session created")
+
+		svc := sns.New(sess)
+		log.Println("service created")
+		msg := fmt.Sprintf("Achtung jemand hat gepusht!")
+
+		params := &sns.PublishInput{
+			Message:     aws.String(msg),
+			PhoneNumber: aws.String(PhoneNumber),
+		}
+		resp, err := svc.Publish(params)
+		if err != nil {
+			log.Println(err.Error())
+			return events.APIGatewayProxyResponse{Body: "error while trying to send sms", StatusCode: http.StatusInternalServerError}, nil
+		}
+		log.Println(resp)
+
+		return events.APIGatewayProxyResponse{Body: "sms will be send", StatusCode: http.StatusOK}, nil
+	}
+
+	return events.APIGatewayProxyResponse{Body: "sms will not be send", StatusCode: http.StatusBadRequest}, nil
 }
 
 func main() {
